ed31f9bca2dcfb9e8c1b1fa37906090caf18c613
melodeenae5 <danaekirby5@gmail.com>
2021-04-21 16:26:28 -0500

    edited server url

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export const apiUrl =
 	window.location.hostname === 'localhost'
 		? 'http://localhost:8082'
-		: 'http://names-and-stuff-server.herokuapp.com/';
+		: 'http://names-and-stuff-server.herokuapp.com';
