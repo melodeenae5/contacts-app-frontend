1e38843b878fecaee321a582ece1588e91470ad2
melodeenae5 <danaekirby5@gmail.com>
2021-04-21 16:19:55 -0500

    added backend server url

--- src/config.js
+++ src/config.js
@@ -1,2 +1,4 @@
 export const apiUrl =
-	window.location.hostname === 'localhost' ? 'http://localhost:8082' : '';
+	window.location.hostname === 'localhost'
+		? 'http://localhost:8082'
+		: 'http://names-and-stuff-server.herokuapp.com/';
